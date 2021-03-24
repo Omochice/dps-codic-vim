f5d5cbc20143736253e7dc67faa9bfa801e8e414
mochi-home <mochi.mls.ntl@gmail.com>
2021-03-25 04:06:56 +0900

    update: add fetch function

--- denops/codic/mod.ts
+++ denops/codic/mod.ts
@@ -1,16 +1,43 @@
-import { start } from "https://deno.land/x/denops_std@v0.2/mod.ts";
+import { start } from "https://deno.land/x/denops_std@v0.4/mod.ts";
+
+async function fetchAPI(text: string[], TOKEN: any): Promise<any> {
+  const BASEURL: string = "https://api.codic.jp/v1/engine/translate.json";
+  const res = await fetch(BASEURL, {
+    headers: new Headers({
+      "Authorization": `Bearer ${TOKEN}`,
+    }),
+    body: new URLSearchParams({
+      "text": text.join("\n"),
+    }),
+    method: "POST",
+  });
+  if (res.status !== 200) {
+    console.log(res.status);
+  }
+  const json = res.json();
+  const data = await json;
+  return data;
+}
 
 start(async (vim) => {
   vim.register({
     async codic(args: unknown): Promise<void> {
       if (typeof args !== "string") {
-        throw new Error(`'opener' must be a string`);
+        throw new Error(`'args' must be a string`);
       }
-      console.log(args);
-      console.log(typeof args);
-      const content = "hello deno and denops world !!";
-      await vim.cmd("new");
-      await vim.call("setline", 1, "hello denops".split(/\r?\n/g));
+      // const content = "hello deno and denops world !!";
+      const TOKEN = await vim.eval("$CODIC_TOKEN");
+
+      const content = args.split(/\s+/);
+
+      await vim.cmd("bo new");
+      // await vim.call("setline", 1, args.split(/\r?\n/g));
+      const data = await fetchAPI(content, TOKEN);
+      await vim.call(
+        "setline",
+        1,
+        JSON.stringify(data, null, 2).split(/\r?\n/g),
+      );
       await vim.execute(`
         setlocal bufhidden=wipe buftype=nofile
         setlocal nobackup noswapfile
