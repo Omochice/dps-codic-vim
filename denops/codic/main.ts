22fed9f9a98c3a8a398538b5fc166a3c3755d147
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-07-21 19:36:56 +0900

    :hammer: use pedit instead of normal window

--- denops/codic/main.ts
+++ denops/codic/main.ts
@@ -7,6 +7,11 @@ import {
   isNumber,
 } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
 
+const config = {
+  "bufname": "codic://output",
+  "filetype": "codic",
+};
+
 async function codic(texts: string[], token: string) {
   if (texts.length >= 4) {
     throw new Error(`[dps-codic-vim] The number of texts must be 3 or less.`);
@@ -52,6 +57,37 @@ function construct(r: any[]): string[] {
   return contents;
 }
 
+async function getExistWin(
+  denops: Denops,
+  bufname: string,
+  opener: string,
+): Promise<number> {
+  let bufnr: number;
+  const bufExist = await denops.call("bufexists", bufname);
+
+  if (bufExist) {
+    bufnr = await denops.call("bufnr", `^${bufname}`) as number;
+    ensureNumber(bufnr);
+    return bufnrToWinId(denops, bufnr);
+  }
+  // i think it ok to use recursive.
+  // because execute open
+  await execute(denops, opener);
+  return await getExistWin(denops, bufname, opener);
+}
+
+async function bufnrToWinId(denops: Denops, bufnr: number): Promise<number> {
+  const wins = await denops.call("win_findbuf", bufnr);
+  ensureArray(wins, isNumber);
+  const tabnr = await denops.call("tabpagenr");
+  ensureNumber(tabnr);
+  const winIds = await denops.eval(
+    `filter(map([${wins}], "win_id2tabwin(v:val)"), "v:val[0] is# ${tabnr}")`,
+  ) as number[][]; // [ [tabnr, winnr] ] TODO replace map(), filter() in typescrit
+  ensureNumber(winIds[0][1]);
+  return winIds[0][1];
+}
+
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async codicVim(args: unknown): Promise<void> {
@@ -70,16 +106,32 @@ export async function main(denops: Denops): Promise<void> {
       const r = await codic(targets, token);
       const lines: string[] = construct(r);
 
-      await denops.cmd("botright new");
+      // make window
+      const currentBufnr = await denops.call("bufnr", "%");
+      ensureNumber(currentBufnr);
+      const opener = `rightbelow pedit ${config["bufname"]}`;
+      await execute(denops, opener);
+      // move window
+      const win = await getExistWin(denops, config["bufname"], opener);
+      await execute(
+        denops,
+        `
+        ${win} wincmd w
+        setlocal modifiable
+        `,
+      );
       await denops.call("setline", 1, lines);
       await execute(
         denops,
         `
-        setlocal bufhidden=wipe buftype=nofile
+        setlocal bufhidden=hide buftype=nofile
         setlocal nobackup noswapfile
         setlocal nomodified nomodifiable
         `,
       );
+      // return original window
+      const currentWinid = await bufnrToWinId(denops, currentBufnr);
+      await execute(denops, `${currentWinid} wincmd w`);
       return await Promise.resolve();
     },
   };
