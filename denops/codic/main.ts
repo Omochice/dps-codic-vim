b985fbb6f59a5cae1bf905b4e813e0be10d70322
mochi-w650z <mochice.mls.ntl@gmail.com>
2021-07-29 02:17:12 +0900

    :hammer: add $ to bufnr

--- denops/codic/main.ts
+++ denops/codic/main.ts
@@ -66,7 +66,7 @@ async function getExistWin(
   const bufExists = (await denops.call("bufexists", bufname) as number == 1);
 
   if (bufExists) {
-    const bufnr = await denops.call("bufnr", `^${bufname}`);
+    const bufnr = await denops.call("bufnr", `^${bufname}$`);
     ensureNumber(bufnr);
     return bufnrToWinId(denops, bufnr);
   }
