5ba43167c2bd876c26c6109a029d5e5c50b50ce1
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-08-23 03:47:55 +0900

    :rocket: update

--- denops/codic/deps.ts
+++ denops/codic/deps.ts
@@ -1,13 +1,5 @@
 export type { Denops } from "https://deno.land/x/denops_std@v1.7.4/mod.ts";
-export {
-  execute,
-  input,
-} from "https://deno.land/x/denops_std@v1.7.4/helper/mod.ts";
+export { input } from "https://deno.land/x/denops_std@v1.7.4/helper/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v1.7.4/variable/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v1.7.4/function/mod.ts";
-export {
-  ensureArray,
-  ensureNumber,
-  ensureString,
-  isNumber,
-} from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
+export { ensureString } from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
--- denops/codic/main.ts
+++ denops/codic/main.ts
@@ -1,24 +1,12 @@
-import {
-  Denops,
-  ensureArray,
-  ensureNumber,
-  ensureString,
-  execute,
-  fn,
-  input,
-  isNumber,
-  vars,
-} from "./deps.ts";
-import { AcronymStyle, Casing, codic, CodicResponce } from "./codic.ts";
+import { Denops, ensureString, fn, input, vars } from "./deps.ts";
+import { AcronymStyle, Casing, codic, CodicResponse } from "./codic.ts";
 
 const config = {
   "bufname": "codic://output",
   "filetype": "codic",
 };
 
-function construct(
-  r: CodicResponce[],
-): string[] {
+function construct(r: CodicResponse[]): string[] {
   const contents: string[] = [];
   for (const datum of r) {
     contents.push(`${datum["text"]} -> ${datum["translated_text"]}`);
@@ -40,37 +28,6 @@ function construct(
   return contents;
 }
 
-async function getExistWin(
-  denops: Denops,
-  bufname: string,
-  opener: string,
-): Promise<number> {
-  const bufExists = await fn.bufexists(denops, bufname);
-
-  if (bufExists) {
-    const bufnr = await fn.bufnr(denops, bufname);
-    ensureNumber(bufnr);
-    return bufnrToWinId(denops, bufnr);
-  }
-  // i think its no problem to use recursive.
-  // because execute open
-  await execute(denops, opener);
-  return await getExistWin(denops, bufname, opener);
-}
-
-async function bufnrToWinId(denops: Denops, bufnr: number): Promise<number> {
-  const wins = await fn.win_findbuf(denops, bufnr);
-  ensureArray(wins, isNumber);
-  const tabnr = await fn.tabpagenr(denops);
-  ensureNumber(tabnr);
-  const winIds =
-    (await denops.eval(`map([${wins}], "win_id2tabwin(v:val)")`) as number[][])
-      .filter((x) => x[0] == tabnr);
-
-  ensureNumber(winIds[0][1]);
-  return winIds[0][1];
-}
-
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async codicVim(args: unknown): Promise<void> {
@@ -84,9 +41,9 @@ export async function main(denops: Denops): Promise<void> {
           denops,
           "dps_codic_acronym_style",
         ) as AcronymStyle,
-      }; // FIXME: if not set vars, return null
+      };
       if (token == undefined) {
-        console.error("[dps-codic-vim] No token set");
+        console.error("[dps-codic-vim] No token is set");
         return await Promise.resolve();
       }
       let response;
@@ -119,30 +76,11 @@ export async function main(denops: Denops): Promise<void> {
 
       const results = construct(response);
 
-      // make window
-      const currentBufnr = await fn.bufnr(denops, "%");
-      const opener = `rightbelow pedit ${config["bufname"]}`;
-      // move window
-      const winId = await getExistWin(denops, config["bufname"], opener);
-      await execute(
-        denops,
-        `
-        ${winId} wincmd w
-        setlocal modifiable
-        `,
-      );
-      await fn.setline(denops, 1, results);
-      await execute(
-        denops,
-        `
-        setlocal bufhidden=hide buftype=nofile
-        setlocal nobackup noswapfile
-        setlocal nomodified nomodifiable
-        `,
-      );
-      // return original window
-      const currentWinId = await bufnrToWinId(denops, currentBufnr);
-      await execute(denops, `${currentWinId} wincmd w`);
+      await denops.cmd(`rightbelow pedit ${config.bufname}`);
+      const bufnr = await fn.bufnr(denops, config.bufname);
+      await fn.setbufvar(denops, bufnr, "&buftype", "nofile");
+      await fn.setbufvar(denops, bufnr, "&filetype", config.filetype);
+      await fn.setbufline(denops, bufnr, 1, results);
       return await Promise.resolve();
     },
   };
