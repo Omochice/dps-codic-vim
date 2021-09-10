cf492bd4ad52a774789d88fe4a29e5361bd2e265
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-09-10 19:16:33 +0900

    :package: update deps

--- denops/codic/deps.ts
+++ denops/codic/deps.ts
@@ -1,5 +1,5 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.8.1/mod.ts";
-export { input } from "https://deno.land/x/denops_std@v1.8.1/helper/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.8.1/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v1.8.1/function/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.9.1/mod.ts";
+export { input } from "https://deno.land/x/denops_std@v1.9.1/helper/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.9.1/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v1.9.1/function/mod.ts";
 export { ensureString } from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
