1a3f476d26da1e84e643e72716dd588d5b0aa6cc
GitHub <noreply@github.com>
2021-10-15 01:13:20 +0000

    :package: update deno dependencies

--- denops/codic/deps.ts
+++ denops/codic/deps.ts
@@ -1,5 +1,5 @@
-export type { Denops } from "https://deno.land/x/denops_std@v2.0.1/mod.ts";
-export { input } from "https://deno.land/x/denops_std@v2.0.1/helper/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v2.0.1/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v2.0.1/function/mod.ts";
-export { ensureString } from "https://deno.land/x/unknownutil@v1.1.3/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v2.1.2/mod.ts";
+export { input } from "https://deno.land/x/denops_std@v2.1.2/helper/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v2.1.2/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v2.1.2/function/mod.ts";
+export { ensureString } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
